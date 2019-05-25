a187ec3346b33036e965afbea6984a2470282902
Victor Nogueira <vmnogueira.ti@gmail.com>
2019-05-24 22:11:04 -0300

    Novidades na tela inicial, mais dicas para os Devs

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
-<?php include ("cabecalho.php"); ?>
-    <div>
-        <h2 class="">Bem vindo a minha loja!</h2>
-    </div>
-<?php include ("rodape.php"); ?>
+<?php include("cabecalho.php"); ?>
+
+
+<div>
+    <h1 style="margin-bottom: 5%;">Bem vindo a minha loja!</h1>    
+</div>
+
+<h3 style="color: tomato; margin-bottom: 5%;"><?php include("conecta.php"); ?></h3>
+
+<div>
+    <h2 style="margin-bottom: 25px;">Dicas pros Devs</h2>
+    <p class="text-muted">Lembre-se de dar um 'DROP DATABASE loja' antes de usar a loja para que atualize a estrutura do BD!</p>
+    <p class="text-muted">Depois do DROP você pode inserir seus produtos normalmente.</p>
+    <br>
+    <h4 style="margin-bottom: 25px;">Se é sua primeira vez acessando e o banco não existe:</h4>
+    <p class="text-muted">Aparecerão mensagens aqui em baixo confirmando a criação do banco.</p>
+
+
+</div>
+<?php include("rodape.php"); ?>
