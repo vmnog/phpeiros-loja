a1c131e7217e646215ab2a8279258e93dc2a4c35
Maicom Ferreira <40906484+maicomferre@users.noreply.github.com>
2019-05-25 08:22:46 -0300

    Adicionando imagem default.
    Adicionando uma imagem que deve substituir a imagem da roupa caso ela não seja enviada/processada corretamente. Imagem temporária.

--- processa-imagem.php
+++ processa-imagem.php
@@ -3,7 +3,7 @@ define('PASTA_IMAGENS','src/produtos/img/',true);
 
 function processaImagem($f):array
 {
-	$name = 'default.jpg';
+	$name = 'default.png';
 	$src = PASTA_IMAGENS;
 	
 	$MIME_Aceitos = array( 'image/jpeg','image/png' );
@@ -16,13 +16,14 @@ function processaImagem($f):array
 	
 	if(is_uploaded_file($f['tmp_name']) === false)die("Erro ao processar a imagem. 0x000003");
 	
-	$ext = explode('.',$f['name']);
-	$ext = end($ext); //Obtem extensão
-	
-	$name = md5(random_bytes(8)).'.'. $ext;
-	$src = PASTA_IMAGENS . DIRECTORY_SEPARATOR . $name ; //Cria md5 aleatório.
-				
-	move_uploaded_file($f['tmp_name'],$src);
+	if(move_uploaded_file($f['tmp_name'],$src) === true){
+		$ext = explode('.',$f['name']);
+		$ext = end($ext); //Obtem extensão
+		
+		$name = md5(random_bytes(8)).'.'. $ext;
+		$src = PASTA_IMAGENS . DIRECTORY_SEPARATOR . $name ; //Cria md5 aleatório.
+					
+	}
 	
 	return array(
 		"src"  => $src,
