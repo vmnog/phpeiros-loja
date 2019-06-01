a7f89fa2a320b10a70cb54d04525f5efa1b355e3
Maicom Ferreira <40906484+maicomferre@users.noreply.github.com>
2019-06-01 03:41:42 -0300

    Drop automatico no banco
    Da um drop automatico no banco de dados, fazendo com que quando se altera o valor de DB_VERSAO automaticamente ela seja deletada, para evitar problemas de compatiblidade.

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
 <?php include("src/includes/cabecalho.php"); ?>
 
+<?php
+	define('DB_VERSAO',1); // 0 , 9999
+	$drop = false;
+	if(!file_exists('dev.ignore')){
+		fclose(fopen('dev.ignore','w'));
+		
+		$drop = true;
+	}
+	else{
+		$f = fopen('dev.ignore','r');
+		$d = (int)fgets($f);
+		fclose($f);
+		
+		$f = fopen('dev.ignore','w');
+		fwrite($f,DB_VERSAO);
+		fclose($f);
+			
+		if($d != DB_VERSAO)$drop = true;
+	}
 
+?>
 <div>
     <h1 style="margin-bottom: 5%;">Bem vindo a minha loja!</h1>    
 </div>
@@ -14,7 +34,14 @@
     <br>
     <h4 style="margin-bottom: 25px;">Se é sua primeira vez acessando e o banco não existe:</h4>
     <p class="text-muted">Aparecerão mensagens aqui em baixo confirmando a criação do banco.</p>
-
+	<br /><br />
+	<?php
+	//Para quando for alterado algo no banco de dados.
+		if($drop){
+			mysqli_query($conexao,'DROP DATABASE loja');
+			echo 'Atenção: <b>DROP DATABASE loja<b>: Automatico! <br /><a href="index.php">Atualize a página para continuar</a>';
+		}
+	?>
 
 </div>
 <?php include("src/includes/rodape.php"); ?>
